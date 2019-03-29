bc53383e205f13fcd219b0968bb92e1f86ff26ef
Chiijlaw <chiijlaw@berkeley.edu>
2019-03-29 15:45:02 -0700

    Have to push bundle for github to serve

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
-const NavBar = require("./components/NavBar.jsx");
+import React from "react";
+import ReactDOM from "react-dom";
+import NavBar from "./components/NavBar.jsx";
 
 const App = () => {
   return (
